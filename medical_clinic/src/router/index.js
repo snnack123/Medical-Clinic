d7ab9418d83297001d55a5e11cdfcc405d09d3e8
Lungu Mihai-Adrian <75578553+snnack123@users.noreply.github.com>
2021-12-03 17:18:44 +0200

    Add files via upload

--- medical_clinic/src/router/index.js
+++ medical_clinic/src/router/index.js
@@ -1,54 +1,55 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
-import Register from "../views/Register.vue";
-import Appointment from "../views/Appointment.vue";
-import Login from "../views/Login.vue";
-import store from "../store";
+import { createRouter, createWebHistory } from 'vue-router'
+import ProductList from '../views/ProductList'
+import Register from '../views/Register'
+import Login from '../views/Login'
+import AddProduct from '../views/AddProduct'
+import store from '../store'
 
-const routes = [{
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
-        path: "/about",
-        name: "About",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ "../views/About.vue"),
-    },
-    {
-        path: "/register",
-        name: "Register",
-        component: Register,
-    },
-    {
-        path: "/login",
-        name: "Login",
-        component: Login,
-    },
-    {
-        path: "/new_appointment",
-        name: "Appointment",
-        component: Appointment,
-    },
-];
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: ProductList
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/addProduct',
+    name: 'AddProduct',
+    component: AddProduct
+  }
+]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-});
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
 
+// Global navigation guard example
 router.beforeEach((to, from, next) => {
-    if (to.name === "Register" || to.name === "Login" || to.name === "Home") {
-        next();
-    } else if (to.name !== "Login" && !store.state.isAuthenticated) {
-        next({ name: "Login" });
-    } else {
-        next();
-    }
-});
+  if (to.name === 'Register' || to.name === 'Login' || to.name === 'Home') {
+    next();
+  } else if (to.name !== 'Login' && !store.state.isAuthenticated) {
+    next({ name: 'Login' });
+  } else {
+    next();
+  }
+})
 
-export default router;
+export default router
