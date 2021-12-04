326c0f200916567b91432828dd0a07cfbb3ddb9d
Lungu Mihai-Adrian <75578553+snnack123@users.noreply.github.com>
2021-12-04 23:07:35 +0200

    Add files via upload

--- medical_clinic/src/store/index.js
+++ medical_clinic/src/store/index.js
@@ -1,69 +1,126 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
-  state: {
-    products: [],
-    isAuthenticated: false,
-  },
-  getters: {
-    products: state => {
-      return state.products;
-    }
-  },
-  mutations: {
-    SET_PRODUCTS: (state, products) => {
-      state.products = products;
+    state: {
+        isAuthenticated: false,
+        client: {},
+        doctors: [],
+        my_doctor: {},
+        all_appointments: [],
+        all_appointments_without_token: [],
     },
-    SET_AUTH: (state, status) => {
-      state.isAuthenticated = status;
-    },
-    ADD_PRODUCT: (state, product) => {
-      state.products.push(product);
-    },
-    DELETE_PRODUCT: (state, id) => {
-      state.products = state.products.filter(element => element.id !== id);
-    },
-    EDIT_PRODUCT: (state, product) => {
-      state.products = state.products.map(element => {       
-         if(element.id === product.id) {       
-          if( element.name !== product.name )    
-            element.name = product.name;
-          if( element.status !== product.status ) {
-            if (product.status === true) {
-              element.status = true;
-            } else {
-              element.status = false;
+    mutations: {
+        SET_AUTH: (state, status) => {
+            state.isAuthenticated = status;
+        },
+        CHECK_AUTHENTICATED: (state) => {
+            if (window.localStorage.getItem("token")) {
+                state.isAuthenticated = true;
             }
-          }           
-         }
-         return element;
-      })
-    },CHECK_AUTHENTICATED: (state) => {
-      if(window.localStorage.getItem('token')){
-        state.isAuthenticated = true;
-      }
-    }
-  },
-  actions: {
-    fetchProducts: ({ commit }, payload) => {
-      commit('SET_PRODUCTS', payload);
+        },
+        SET_EMAIL: (state, status) => {
+            state.client.email = status;
+        },
+        SET_CLIENT: (state, status) => {
+            state.client.id = status.id;
+            state.client.name = status.name;
+            state.client.phone = status.phone;
+            state.client.yearsOld = status.yearsOld;
+        },
+        SET_DOCTORS: (state, status) => {
+            state.doctors = status;
+        },
+        SET_MY_DOCTOR: (state, status) => {
+            state.my_doctor.name = status.name;
+            state.my_doctor.specialization = status.specialization;
+        },
+        SET_ALL_APPOINTMENTS: (state, status) => {
+            while (state.all_appointments.length) state.all_appointments.pop();
+            state.all_appointments = status;
+        },
+        SET_ALL_APPOINTMENTS_WITHOUT_TOKEN: (state, status) => {
+            while (state.all_appointments_without_token.length) state.all_appointments.pop();
+            state.all_appointments_without_token = status;
+        },
+        ADD_APPOINTMENT: (state, status) => {
+            state.all_appointments.push(status);
+        },
+        DELETE_APPOINTMENT: (state, status) => {
+            state.all_appointments = state.all_appointments.filter(element => element.status !== status);
+        },
+        CLEAR_ALL: (state) => {
+            state.isAuthenticated = false;
+            for (const prop of Object.getOwnPropertyNames(state.client)) {
+                delete state.client[prop];
+            }
+            for (const prop of Object.getOwnPropertyNames(state.my_doctor)) {
+                delete state.client[prop];
+            }
+            while (state.doctors.length) state.doctors.pop();
+            while (state.all_appointments.length) state.all_appointments.pop();
+        },
+        CLEAR_APP_WITHOUT_TOKEN: (state) => {
+            while (state.all_appointments_without_token.length) state.all_appointments_without_token.pop();
+        },
+        EDIT_APPOINTMENT: (state, status) => {
+            for (let i = 0; i < state.all_appointments.length; i++) {
+                if (state.all_appointments[i].id === status.id) {
+                    state.all_appointments[i].price = status.price;
+                }
+            }
+        },
+        EDIT_CLIENT: (state, status) => {
+            if (state.client.yearsOld !== status.yearsOld) {
+                state.client.yearsOld = status.yearsOld;
+            }
+            if (state.client.phone !== status.yearsOld) {
+                state.client.phone = status.phone;
+            }
+        },
     },
-    login: ({ commit }, payload) => {
-      commit('SET_AUTH', payload);
+    actions: {
+        login: ({ commit }, payload) => {
+            commit("SET_AUTH", payload);
+        },
+        checkAuthenticated: ({ commit }) => {
+            commit("CHECK_AUTHENTICATED");
+        },
+        setEmailClient: ({ commit }, payload) => {
+            commit("SET_EMAIL", payload);
+        },
+        setClient: ({ commit }, payload) => {
+            commit("SET_CLIENT", payload);
+        },
+        setDoctors: ({ commit }, payload) => {
+            commit("SET_DOCTORS", payload);
+        },
+        setMyDoctor: ({ commit }, payload) => {
+            commit("SET_MY_DOCTOR", payload);
+        },
+        setAllAppointments: ({ commit }, payload) => {
+            commit("SET_ALL_APPOINTMENTS", payload);
+        },
+        setAllAppointmentsWithoutToken: ({ commit }, payload) => {
+            commit("SET_ALL_APPOINTMENTS_WITHOUT_TOKEN", payload);
+        },
+        addAppointment: ({ commit }, payload) => {
+            commit("ADD_APPOINTMENT", payload);
+        },
+        deleteAppointment: ({ commit }, payload) => {
+            commit('DELETE_APPOINTMENT', payload);
+        },
+        clearAll: ({ commit }) => {
+            commit('CLEAR_ALL');
+        },
+        clearAllAppointmentsWithoutToken: ({ commit }) => {
+            commit('CLEAR_APP_WITHOUT_TOKEN');
+        },
+        editAppointment: ({ commit }, payload) => {
+            commit('EDIT_APPOINTMENT', payload);
+        },
+        editClient: ({ commit }, payload) => {
+            commit('EDIT_CLIENT', payload);
+        },
     },
-    addProduct: ({ commit }, payload) => {
-      commit('ADD_PRODUCT', payload);
-    },
-    deleteProduct: ({ commit }, payload) => {
-      commit('DELETE_PRODUCT', payload);
-    },
-    editProduct: ({commit}, payload) => {
-      commit('EDIT_PRODUCT', payload);
-    },
-    checkAuthenticated: ({commit}) => {
-      commit('CHECK_AUTHENTICATED');
-    }
-  },
-  modules: {
-  }
-})
+    modules: {},
+});
